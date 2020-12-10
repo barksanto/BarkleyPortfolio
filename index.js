d089b5bfe9afc3dd75e8e5c314f45d7e5f1fbb88
Barkley Santo <barkley.santo@gmail.com>
2020-12-10 07:37:16 -0600

    added typed js and positioned top

---
+++ index.js
@@ -0,0 +1,10 @@
+import Typed from 'typed.js';
+
+var options = {
+  strings: ['Welcome to my page.', "Let's build something"],
+  typeSpeed: 40
+};
+
+var typed = new Typed('.element', options);
+
+

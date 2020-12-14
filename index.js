ca9245af2e3b3b906250951afefc9d2955ce9626
Barkley Santo <barkley.santo@gmail.com>
2020-12-14 15:47:02 -0600

    cards are open on smaller devices

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
 import Typed from 'typed.js';
 
-var options = {
-  strings: ['Welcome to my page.', "Let's build something"],
-  typeSpeed: 40
-};
-
-var typed = new Typed('.element', options);
-
-

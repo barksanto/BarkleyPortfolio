e757cb0a4589e8d2d6104c35aff805b28b6bdb5b
Barkley <barkley.santo@gmail.com>
2022-05-12 18:47:00 -0500

    collapse nav on item click

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,13 @@
 const navList = document.getElementById("navbarNavAltMarkup");
+const body = document.getElementById("top");
 
 const navItems = document.querySelectorAll(".navbar-nav > .nav-item");
 navItems.forEach((navitem) => {
   navitem.addEventListener("click", () => {
     navList.classList.remove("show");
   });
+});
+
+body.addEventListener("click", () => {
+  navList.classList.remove("show");
 });
